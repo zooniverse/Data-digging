6415cb36f8ec8894b550887a4caadce80a91e204
lcjohnso <lcjohnso@astro.washington.edu>
2017-01-26 16:12:00 -0800

    added development folder

---
+++ development/export_markings_and_classifications.py
@@ -0,0 +1,13 @@
+# -------------------------------------------------------------
+# export_markings_and_classifications
+# A generalized script for exporting flattened CSV data
+# 
+# Take classification and workflow data export files, and 
+# export flattened CSVs for classifications and markings.
+# 
+# Initialized by: Cliff Johnson (lcj@ucsd.edu)
+# -------------------------------------------------------------
+
+### Work in progress!
+
+# Marker Types
