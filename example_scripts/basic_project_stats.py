c02c69461ea2fbf9dfab22ccef4824a74c2f8bba
Brooke Simmons <vrooje@gmail.com>
2017-03-29 19:46:11 -0700

    upgrading basic_project_stats.py
    you can now specify a workflow id/version; also can compute total time (work effort) spent classifying

--- example_scripts/basic_project_stats.py
+++ example_scripts/basic_project_stats.py
@@ -7,20 +7,45 @@ import sys
 try:
     classfile_in = sys.argv[1]
 except:
-    print "\nUsage: "+sys.argv[0]+" classifications_infile"
-    print "      classifications_infile is a Zooniverse (Panoptes) classifications data export CSV."
-    print "\nAll output will be to stdout (about a paragraph worth).\n"
+    print("\nUsage: %s classifications_infile" % sys.argv[0])
+    print("      classifications_infile is a Zooniverse (Panoptes) classifications data export CSV.")
+    print("  Optional inputs (no spaces):")
+    print("    workflow_id=N")
+    print("       specify the program should only consider classifications from workflow id N")
+    print("    workflow_version=M")
+    print("       specify the program should only consider classifications from workflow version M")
+    print("       (note the program will only consider the major version, i.e. the integer part)")
+    print("    --time_elapsed")
+    print("       specify the program should compute classification durations and total classification work effort")
+    print("\nAll output will be to stdout (about a paragraph worth).\n")
     sys.exit(0)
 
 
 
 import numpy as np  # works in 1.10.1
 import pandas as pd  # works in 0.13.1
-#import datetime
-#import dateutil.parser
+import datetime
+import dateutil.parser
 import json
 
+# default value is not to care about workflow ID or version
+workflow_id      = -1
+workflow_version = -1
+# by default we won't worry about computing how much time effort the volunteers cumulatively spent
+time_elapsed = False
 
+# check for other command-line arguments
+if len(sys.argv) > 2:
+    # if there are additional arguments, loop through them
+    for i_arg, argstr in enumerate(sys.argv[2:]):
+        arg = argstr.split('=')
+
+        if arg[0]   == "workflow_id":
+            workflow_id = int(arg[1])
+        elif arg[0] == "workflow_version":
+            workflow_version = float(arg[1])
+        elif arg[0] == "--time_elapsed":
+            time_elapsed = True
 
 
 
@@ -54,8 +79,8 @@ import json
 
 
 # Print out the input parameters just as a sanity check
-print "Computing project stats using:"
-print "   infile:",classfile_in
+print("Computing project stats using:")
+print("   infile: %s" % classfile_in)
 
 
 
@@ -117,16 +142,58 @@ def gini(list_of_values):
 # Begin the main stuff
 
 
-print "Reading classifications from "+classfile_in
+print("Reading classifications from %s" % classfile_in)
 
 classifications = pd.read_csv(classfile_in)
 
+classifications['version_int'] = [int(q) for q in classifications.workflow_version]
+
+if (workflow_id > 0) | (workflow_version > 0):
+    # only keep the stuff that matches these workflow properties
+    if (workflow_id > 0):
+
+        print("Considering only workflow id %d" % workflow_id)
+
+        in_workflow = classifications.workflow_id == workflow_id
+    else:
+        # the workflow id wasn't specified, so just make an array of true
+        in_workflow = np.array([True for q in classifications.workflow_id])
+
+    if (workflow_version > 0):
+
+        print("Considering only major workflow version %d" % int(workflow_version))
+
+        # we only care about the major workflow version, not the minor version
+        in_version = classifications.version_int == int(workflow_version)
+    else:
+        in_version = np.array([True for q in classifications.workflow_version])
+
+
+    if (sum(in_workflow & in_version) == 0):
+        print("ERROR: your combination of workflow_id and workflow_version does not exist!\nIgnoring workflow id/version request and computing stats for ALL classifications instead.")
+        #classifications = classifications_all
+    else:
+        # select the subset of classifications
+        classifications = classifications[in_workflow & in_version]
+
+
+else:
+    # just use everything
+    #classifications = classifications_all
+
+    workflow_ids = classifications.workflow_id.unique()
+    classifications['version_int'] = [int(q) for q in classifications.workflow_version]
+    version_ints = classifications.version_int.unique()
+
+    print("Considering all classifications in workflow ids:")
+    print(workflow_ids)
+    print(" and workflow_versions (major only):")
+    print(version_ints)
+
+
+
 # first, extract the started_at and finished_at from the annotations column
 classifications['meta_json'] = [json.loads(q) for q in classifications.metadata]
-
-
-classifications['started_at_str']  = [q['started_at']  for q in classifications.meta_json]
-classifications['finished_at_str'] = [q['finished_at'] for q in classifications.meta_json]
 
 classifications['created_day'] = [q[:10] for q in classifications.created_at]
 
@@ -183,14 +250,71 @@ nclass_mean   = np.mean(nclass_byuser)
 # Gini coefficient - see the comments above the gini() function for more notes
 nclass_gini   = gini(nclass_byuser)
 
-print "\nOverall:\n\n",n_class_tot,"classifications of",n_subj_tot,"subjects by",n_users_tot,"classifiers,"
-print n_reg,"registered and",n_unreg,"unregistered.\n"
-print "That's %.2f classifications per subject on average (median = %.1f)." % (subj_class_mean, subj_class_med)
-print "The most classified subject has ",subj_class_max,"classifications; the least-classified subject has",subj_class_min,".\n"
-print "Median number of classifications per user:",nclass_med
-print "Mean number of classifications per user: %.2f" % nclass_mean
-print "\nTop 10 most prolific classifiers:\n",nclass_byuser_ranked.head(10)
-print "\n\nGini coefficient for classifications by user: %.2f\n" % nclass_gini
+print("\nOverall:\n\n%d classifications of %d subjects by %d classifiers," % (n_class_tot,n_subj_tot,n_users_tot))
+print("%d registered and %d unregistered.\n" % (n_reg,n_unreg))
+print("That's %.2f classifications per subject on average (median = %.1f)." % (subj_class_mean, subj_class_med))
+print("The most classified subject has %d classifications; the least-classified subject has %d.\n" % (subj_class_max,subj_class_min))
+print("Median number of classifications per user: %.2f" %nclass_med)
+print("Mean number of classifications per user: %.2f" % nclass_mean)
+print("\nTop 10 most prolific classifiers:")
+print(nclass_byuser_ranked.head(10))
+print("\n\nGini coefficient for classifications by user: %.2f" % nclass_gini)
+print("\nClassifications were collected between %s and %s.\n" % (first_class_day, last_class_day))
 
 
-# That's it. This program is very basic.
+# if the input specified we should compute total time spent by classifiers, compute it
+if time_elapsed:
+
+    classifications['started_at_str']  = [q['started_at'].replace('T',' ').replace('Z', '')  for q in classifications.meta_json]
+    classifications['finished_at_str'] = [q['finished_at'].replace('T',' ').replace('Z', '') for q in classifications.meta_json]
+
+    sa_temp = classifications['started_at_str']
+    fa_temp = classifications['finished_at_str']
+
+    #print("Creating timeseries...")#,datetime.datetime.now().strftime('%H:%M:%S.%f')
+
+
+    try:
+        classifications['started_at'] = pd.to_datetime(sa_temp, format='%Y-%m-%d %H:%M:%S.%f')
+    except Exception as the_error:
+        print("Oops:\n%s" % the_error)
+        try:
+            classifications['started_at'] = pd.to_datetime(sa_temp, format='%Y-%m-%d %H:%M:%S %Z')
+        except Exception as the_error:
+            print("Oops:\n%s" % the_error)
+            classifications['started_at'] = pd.to_datetime(sa_temp)
+
+
+    try:
+        classifications['finished_at'] = pd.to_datetime(fa_temp, format='%Y-%m-%d %H:%M:%S.%f')
+    except Exception as the_error:
+        print("Oops:\n%s" % the_error)
+        try:
+            classifications['finished_at'] = pd.to_datetime(fa_temp, format='%Y-%m-%d %H:%M:%S %Z')
+        except Exception as the_error:
+            print("Oops:\n%s" % the_error)
+            classifications['finished_at'] = pd.to_datetime(fa_temp)
+
+
+    classifications['class_t_length'] = (classifications.finished_at - classifications.started_at)
+
+    # throw away absurd time counts: accept lengths between 0 < dt < 30 minutes
+    # anything outside that is either a wrongly reported time or the user walked away from their computer
+
+    ok_times = (classifications.class_t_length > np.timedelta64(0, 's')) & (classifications.class_t_length < np.timedelta64(30, 'm'))
+
+    n_t_ok = sum(ok_times)
+
+    time_spent_classifying = np.sum(classifications['class_t_length'][ok_times])
+
+    print("Based on %d classifications (%.1f) where we can probably trust the classification durations, \nthe classifiers spent a total of %.2f days classifying in the project." % (n_t_ok, float(n_t_ok)/float(n_class_tot)*100., time_spent_classifying / np.timedelta64(1, 'D')))
+
+    mean_t_class   =   np.mean(classifications['class_t_length'][ok_times])
+    median_t_class = np.median(classifications['class_t_length'][ok_times])
+
+    print("Mean classification length: %8.1f seconds"   % float(mean_t_class   / np.timedelta64(1, 's')))
+    print("Median classification length: %6.1f seconds" % float(median_t_class / np.timedelta64(1, 's')))
+
+
+
+#end
