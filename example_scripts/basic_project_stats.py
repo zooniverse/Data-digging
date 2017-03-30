5fc107f1647b5fa58eb41a2726ec412305733f53
Brooke Simmons <vrooje@gmail.com>
2017-03-30 15:12:39 -0700

    updates to basic_project_stats
    added FTE calculation, plus some better memory usage

--- example_scripts/basic_project_stats.py
+++ example_scripts/basic_project_stats.py
@@ -27,6 +27,7 @@ import pandas as pd  # works in 0.13.1
 import datetime
 import dateutil.parser
 import json
+import gc
 
 # default value is not to care about workflow ID or version
 workflow_id      = -1
@@ -50,7 +51,7 @@ if len(sys.argv) > 2:
 
 
 # columns currently in an exported Panoptes classification file:
-# classification_id,user_name,user_id,user_ip,workflow_id,workflow_name,workflow_version,created_at,gold_standard,expert,metadata,annotations,subject_data
+# classification_id,user_name,user_id,user_ip,workflow_id,workflow_name,workflow_version,created_at,gold_standard,expert,metadata,annotations,subject_data,subject_ids
 
 # classification_id identifies the specific classification - should be unique for each row in this file
 # user_name is either their registered name or "not-logged-in"+their hashed IP
@@ -71,11 +72,10 @@ if len(sys.argv) > 2:
 # annotations (json) contains the actual classification information
 #       which for this analysis we will ignore completely, for now
 # subject_data is cross-matched from the subjects table and is for convenience in data reduction
+# subject_ids has just the subject ids in the given classification
 #       here we will ignore this too, except to count subjects once.
 # we'll also ignore classification_id, user_ip, workflow information, gold_standard, and expert.
 #
-# some of these will be defined further down, but before we actually use this list.
-#cols_used = ["created_at_ts", "user_name", "user_id", "created_at", "started_at", "finished_at"]
 
 
 # Print out the input parameters just as a sanity check
@@ -144,7 +144,21 @@ def gini(list_of_values):
 
 print("Reading classifications from %s" % classfile_in)
 
-classifications = pd.read_csv(classfile_in)
+#classifications = pd.read_csv(classfile_in)
+# the above will work but uses a LOT of memory for projects with > 1 million
+# classifications. Nothing here uses the actual classification data so don't read it
+'''
+If you are using this code on an older project, where the data export is from
+before subject_ids were exported as their own column, change "subject_id" below
+to "subject_data", and then when you define the groupby "by_subject" and count
+subjects, you'll need to use subject_data instead of subject_ids.
+
+Apologies for doing this, but subject_data contains the whole manifest so for
+big projects with big catalogs it can take up a lot of memory, so we don't want to
+use it if we don't have to.
+'''
+cols_keep = ["user_name", "user_id", "workflow_id", "workflow_version", "created_at", "metadata", "subject_ids"]
+classifications = pd.read_csv(classfile_in, usecols=cols_keep)
 
 classifications['version_int'] = [int(q) for q in classifications.workflow_version]
 
@@ -192,7 +206,7 @@ else:
 
 
 
-# first, extract the started_at and finished_at from the annotations column
+# first, extract the started_at and finished_at from the metadata column
 classifications['meta_json'] = [json.loads(q) for q in classifications.metadata]
 
 classifications['created_day'] = [q[:10] for q in classifications.created_at]
@@ -201,10 +215,15 @@ first_class_day = min(classifications.created_day).replace(' ', '')
 last_class_day  = max(classifications.created_day).replace(' ', '')
 
 
+# save processing time and memory in the groupby.apply(); only keep the columns we're going to use
+cols_used = ["user_name", "user_id", "created_at", "created_day", "meta_json", "subject_ids"]
+classifications = classifications[cols_used]
+# collect() calls PyInt_ClearFreeList(), so explicitly helps free some active memory
+gc.collect()
 
 # grab the subject counts
-n_subj_tot  = len(classifications.subject_data.unique())
-by_subject = classifications.groupby('subject_data')
+n_subj_tot  = len(classifications.subject_ids.unique())
+by_subject = classifications.groupby('subject_ids')
 subj_class = by_subject.created_at.aggregate('count')
 
 # basic stats on how classified the subjects are
@@ -213,8 +232,11 @@ subj_class_med  = np.median(subj_class)
 subj_class_min  = np.min(subj_class)
 subj_class_max  = np.max(subj_class)
 
-# save processing time and memory in the groupby.apply(); only keep the columns we're going to use
-#classifications = classifications[cols_used]
+# free up some memory - note calling this does take CPU time but
+# can free up GBs of active memory for big classification files
+del by_subject
+gc.collect()
+
 
 # index by created_at as a timeseries
 # note: this means things might not be uniquely indexed
@@ -241,7 +263,7 @@ n_reg   = n_users_tot - n_unreg
 # e.g. whether they're also your most prolific Talk users
 nclass_byuser = by_user.created_at.aggregate('count')
 nclass_byuser_ranked = nclass_byuser.copy()
-nclass_byuser_ranked.sort(ascending=False)
+nclass_byuser_ranked.sort_values(inplace=True, ascending=False)
 
 # very basic stats
 nclass_med    = np.median(nclass_byuser)
@@ -264,6 +286,12 @@ print("\nClassifications were collected between %s and %s.\n" % (first_class_day
 
 # if the input specified we should compute total time spent by classifiers, compute it
 if time_elapsed:
+    # free up some memory
+    # do this inside the if because if we're not computing times then the program
+    # is about to end so this memory will be freed up anyway
+    del by_user
+    gc.collect()
+
 
     classifications['started_at_str']  = [q['started_at'].replace('T',' ').replace('Z', '')  for q in classifications.meta_json]
     classifications['finished_at_str'] = [q['finished_at'].replace('T',' ').replace('Z', '') for q in classifications.meta_json]
@@ -302,17 +330,27 @@ if time_elapsed:
     # anything outside that is either a wrongly reported time or the user walked away from their computer
     ok_times = (classifications.class_t_length > np.timedelta64(0, 's')) & (classifications.class_t_length < np.timedelta64(30, 'm'))
 
+    # how many turned out to be okay?
     n_t_ok = sum(ok_times)
 
+    # compute total times
     time_spent_classifying = np.sum(classifications['class_t_length'][ok_times])
+    days_spent_classifying = time_spent_classifying / np.timedelta64(1, 'D')
+    frac_good_durations = float(n_t_ok)/float(n_class_tot)
 
-    print("Based on %d classifications (%.1f\%) where we can probably trust the\nclassification durations, the classifiers spent a total of %.2f days classifying\nin the project." % (n_t_ok, float(n_t_ok)/float(n_class_tot)*100., time_spent_classifying / np.timedelta64(1, 'D')))
+    print("Based on %d classifications (%.1f percent) where we can probably\ntrust the classification durations, the classifiers spent a total of %.2f days\n(or %.2f years) classifying in the project.\n" % (n_t_ok, frac_good_durations*100., days_spent_classifying, days_spent_classifying / 365.))
 
     mean_t_class   =   np.mean(classifications['class_t_length'][ok_times])
     median_t_class = np.median(classifications['class_t_length'][ok_times])
 
+    human_effort_extrap = float(n_class_tot)*float(mean_t_class   / np.timedelta64(1, 'D')) / 365. # in years
+
     print("Mean classification length: %8.1f seconds"   % float(mean_t_class   / np.timedelta64(1, 's')))
     print("Median classification length: %6.1f seconds" % float(median_t_class / np.timedelta64(1, 's')))
+
+
+
+    print("\nIf we use the mean to extrapolate and include the %.1f percent of\nclassifications where the reported duration had an error, that means\nthe total time spent is equivalent to %.2f years of human effort, or\n%.2f years of FTE (1 person working 40 hours/week, no holiday.)\n" % ((1-frac_good_durations)*100., human_effort_extrap, human_effort_extrap * (24.*7.)/40.))
 
 
 
