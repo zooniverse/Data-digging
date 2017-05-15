2f3227baeca3674338191dc6b6b862fc28443c59
Brooke Simmons <vrooje@gmail.com>
2017-05-14 17:54:47 -0700

    return classification columns function

--- example_scripts/aggregate_question_utils.py
+++ example_scripts/aggregate_question_utils.py
@@ -2,15 +2,26 @@ import sys, os, glob
 import pandas as pd, numpy as np
 import ujson
 import datetime
-from get_workflow_info import get_workflow_info, translate_non_alphanumerics
+from get_workflow_info import get_workflow_info, translate_non_alphanumerics, get_short_slug
 
-def breakout_anno(row, workflow_info):
+
+################################################################################
+
+#    Jailbreak question annotations from their JSON
+#    (will partially jailbreak markings etc, but not fully)
+
+################################################################################
+
+def breakout_anno_q(row, workflow_info):
     # if you're doing this by iterating yourself and feeding it a row it needs
     # to be row[1]['anno_json'] because row[0] is the row index
     # but if you're calling this by a .apply(lambda ) it doesn't have that
     # because obviously why would you want them to have the same syntax why why why
     annotations = row['anno_json']
 
+    # I was trying this with numpy in hopes of saving time but that can lead to ordering problems
+    # just saving it here in case I ever change my mind
+    #
     # theclass = np.empty(len(d_cols), dtype=object)
     # the_cols = np.empty(len(d_cols), dtype=object)
     #
@@ -26,12 +37,13 @@ def breakout_anno(row, workflow_info):
 
 
     theclass = {}
-    for thistask in annotations:
+    for task in annotations:
         try:
-            theclass[workflow_info[thistask['task']+'_shorttext']] = thistask['value']
+            theclass[workflow_info[task['task']+'_shorttext']] = task['value']
         except:
-            theclass[workflow_info[thistask['task']+'_shorttext']] = str(thistask['value'])
+            theclass[workflow_info[task['task']+'_shorttext']] = str(task['value'])
 
+    # if things are going very badly, uncomment these and pipe the output to a logfile
     # print("------------------------------------------------------------")
     # print(row)
     # print(theclass)
@@ -41,6 +53,120 @@ def breakout_anno(row, workflow_info):
     return pd.Series(theclass)
 
 
+
+################################################################################
+
+#    Jailbreak survey annotations from their JSON
+
+################################################################################
+def breakout_anno_survey(row, workflow_info, fp, classcols, thecols):
+    annotations = row['anno_json']
+    #classcols = "classification_id created_at user_name user_id user_ip".split()
+    printcols = classcols + thecols
+
+    n_marks = 0
+    theclass = {}
+
+    # fill the row with the basic classification information
+    # theclass['classification_id'] = row.index
+    # for col in "created_at user_name user_id user_ip".split():
+    #     theclass[col] = row[col]
+    # actually, let's assume we haven't set classification_id to be the index
+    for col in classcols:
+        theclass[col] = row[col]
+
+    # create all the other relevant columns
+    for col in thecols:
+        theclass[col] = ''
+
+
+    for task in annotations:
+        taskname = task['task']
+        tasktype = workflow_info[taskname]['type']
+
+        # for a survey we expect a survey task and a "shortcut" for e.g.
+        # "Nothing Here", and they require different approaches
+        # either way we'll write 1 row per mark to the file
+
+        if tasktype == "survey":
+            marks = task['value']
+            for mark in marks:
+                n_marks += 1
+
+                # empty the dict of marks
+                for col in thecols:
+                    theclass[col] = ''
+
+                # fill in the dict
+                theclass[taskname.lower()+'_choice'] = mark['choice']
+                for ans in mark['answers'].keys():
+                    thelabel = workflow_info[taskname]['questions'][ans]['label_slug']
+                    #thelabel = get_short_slug(ans)
+                    theclass[taskname.lower()+'_'+thelabel] = mark['answers'][ans]
+                # not currently doing anything with "filters"
+
+                # print the mark
+                write_class_row(fp, theclass, printcols)
+
+        elif tasktype == "shortcut":
+
+            n_marks += 1
+
+            # empty the dict of marks
+            for col in thecols:
+                theclass[col] = ''
+
+            # populate a default value for all the relevant columns
+            for ans in workflow_info[taskname]['answers']:
+                theclass[ans['label_slug']] = False
+
+            # now populate the ones we have actual info for
+            for ans_orig in task['value']:
+                # get the index in the workflow answer map so we can fetch
+                # the correct column label
+                i_a = workflow_info[taskname]['answer_map'][ans_orig]
+                ans = workflow_info[taskname]['answers'][i_a]['label_slug']
+                #ans = get_short_slug(ans_orig.lower())
+                theclass[ans] = True
+
+            # now write the row to the file
+            write_class_row(fp, theclass, printcols)
+
+    return n_marks
+
+
+################################################################################
+
+#    Write a dictionary to a csv using columns and order in thecols
+
+################################################################################
+
+def write_class_row(fp, theclass, thecols):
+    # print the row
+    for i in range(len(thecols)):
+        entry = theclass[thecols[i]]
+        if not i == 0:
+            fp.write(",")
+        try:
+            if isinstance(entry, (list, tuple)):
+                fp.write('"%s"' % str(entry))
+            else:
+                fp.write(str(entry))
+        except:
+            pass
+    fp.write("\n")
+
+    return
+
+
+
+
+################################################################################
+
+#    Compute a vote fraction
+
+################################################################################
+
 def getfrac(row, colname, colcount):
     try:
         return float(row[colname])/float(row[colcount])
@@ -49,6 +175,13 @@ def getfrac(row, colname, colcount):
 
 
 
+
+
+################################################################################
+
+#    Aggregate question vote fractions based on a dictionary of tasks
+
+################################################################################
 
 def aggregate_questions(classifications, theqdict, verbose=True):
 
