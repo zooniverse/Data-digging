eccdef4c4264399769ee1fcc2a94a34703bb3cc4
Brooke Simmons <vrooje@gmail.com>
2018-05-23 16:56:04 -0700

    look up credited_name with author list

--- example_scripts/make_author_list.py
+++ example_scripts/make_author_list.py
@@ -19,6 +19,8 @@ except:
     print("       output the list in pre-formatted tags so markdown won't render")
     print("    col=col_name")
     print("       if the name of your Author name column isn't standard, specify it")
+    print("    --no_lookup, --nolookup")
+    print("       Skip attempted lookup of credited_name (if not already supplied)")
     print("    len=N  (or length=N, line=N)")
     print("       If pre-formatted text, specifies max length of each line (default 72).")
     print("    outcsv=output_of_logged_in_users_only.csv")
@@ -28,7 +30,8 @@ except:
 
 
 
-def clean_email_str(str):
+def clean_email_str(thevalue):
+    thestr = str(thevalue)
     # alas there are plenty of . characters that shouldn't be replaced
     # so we have to be more specific
     thiswiththat = [
@@ -61,11 +64,11 @@ def clean_email_str(str):
     ]
 
     # if there isn't a ., save some time
-    if "." in str:
+    if "." in thestr:
         for tt in thiswiththat:
-            str = str.replace(tt[0], tt[1])
+            thestr = thestr.replace(tt[0], tt[1])
 
-    return str
+    return thestr
 
 
 
@@ -102,6 +105,91 @@ def get_best_name(row, author_col, author_col_backup):
     return str(author)
 
 
+def get_credited_name_all(vols, whichtype='user_id'):
+    x = panoptes_connect()
+
+    disp_names = vols.copy()
+    i_print = int(len(vols)/10)
+    if i_print > 1000:
+        i_print = 1000
+
+    if whichtype == 'user_id':
+        # user ID lookup has a different format for the query
+        for i, the_id in enumerate(vols):
+            user = User.find(int(the_id))
+            name_use = user.credited_name
+            if name_use == '':
+                name_use = user.display_name
+                if name_use == '':
+                    name_use = user.login
+
+            disp_names.loc[(vols==the_id)] = name_use
+            #credited_names[the_id] = user.credited_name
+            if i % i_print == 0:
+                print("Credited name: lookup %d of %d (%s --> %s)" % (i, len(vols), the_id, name_use))
+
+    else:
+        # if we're here we don't have user ID but presumably do have login
+        for i, the_login in enumerate(vols):
+            name_use = the_login
+            for user in User.where(login=the_login):
+                name_use = user.display_name
+            #credited_names[the_login] = cname
+            disp_names.loc[(vols==the_login)] = name_use
+            if i % i_print == 0:
+                print("Credited name: lookup %d of %d (%s --> %s)" % (i, len(vols), the_login, name_use))
+
+
+
+    return disp_names
+
+
+
+
+
+
+def get_credited_name(row, author_col, author_col_backup):
+    x = panoptes_connect()
+
+
+    # try standard columns plus whatever was supplied (if it's different)
+    loginname_cols = ['user_name', 'username', 'login']
+    # if author columns are supplied, try those first
+    if (not (author_col_backup.isin(loginname_cols))) & (not (author_col_backup == '')):
+        loginname_cols = [author_col_backup] + loginname_cols
+
+    if (not (author_col.isin(loginname_cols))) & (not (author_col == '')):
+        loginname_cols = [author_col] + loginname_cols
+
+    try:
+        userid = row[1]['user_id']
+        user = User.find(int(userid))
+        return user.credited_name
+
+    except:
+        login_col = ''
+        for i_col in range(len(loginname_cols)):
+            if loginname_cols[i_col] in row[1].keys():
+                login_col = loginname_cols[i_col]
+                break
+
+        # if you only know the login you have to search panoptes_client in this way
+        for user in User.where(login=login_col):
+            return user.credited_name
+
+
+        if login_col == '':
+            # if you're here neither the user_id nor any of the login columns worked
+            return login_col
+
+        # you should never get here but hey ho
+        return login_col
+
+
+
+
+
+
 
 
 
@@ -110,6 +198,7 @@ clean_emails = False
 preformat = False
 usecol_cl = False
 out_logged_in = False
+skip_lookup = False
 
 # matters if preformatted, if not it's just to make the file itself easier to read
 max_line_length_char = 72
@@ -124,6 +213,8 @@ if len(sys.argv) > 2:
             clean_emails = True
         elif (arg[0] == "--pre") | (arg[0] == "--preformatted") | (arg[0] == "--preformat"):
             preformat = True
+        elif (arg[0] == "--no_lookup") | (arg[0] == "--nolookup"):
+            skip_lookup = True
         elif (arg[0] == "col") | (arg[0] == "usecol"):
             usecol_cl = True
             author_col = arg[1]
@@ -133,10 +224,17 @@ if len(sys.argv) > 2:
             out_logged_in = True
             outcsv = arg[1]
 
+# you might be skipping the lookup because you don't have this installed
+if not skip_lookup:
+    from panoptes_client import User
+    from panoptes_connect import panoptes_connect
+
+
 
 # Ideally we'd use the user-supplied credited name, but if not available use their username
 cols_to_try_preferred = ['credited_name', 'real_name', 'display_name']
-cols_to_try_backup = ['user_name', 'username', 'login', 'user_id']
+login_cols = ['user_name', 'username', 'login']
+cols_to_try_backup = login_cols + ['user_id']
 # use the first column name if it exists, then the second, etc.
 cols_to_try_ranked = ['name_merged'] + cols_to_try_preferred + cols_to_try_backup
 
@@ -196,6 +294,17 @@ if author_col in cols_to_try_preferred:
     # deal with blank user-supplied columns
     authorlist['name_merged'] = [get_best_name(q, author_col, author_col_backup) for q in authorlist.iterrows()]
     author_col = 'name_merged'
+# now, try to look up the credited name if we don't already have it
+elif ('user_id' in authorlist.columns) & (not skip_lookup):
+    authorlist['name_merged'] = get_credited_name_all(authorlist['user_id'])
+    author_col = 'name_merged'
+else:
+    if not skip_lookup:
+        for thename in login_cols:
+            if thename in authorlist.columns:
+                authorlist['name_merged'] = get_credited_name_all(authorlist[thename], whichtype=thename)
+                author_col = 'name_merged'
+                break
 
 
 # these are unnecessary if you're running this from a prompt but if you're copy-pasting in iPython they're needed so things below don't break
@@ -223,10 +332,17 @@ linestr = prepend
 
 for i_line, row in enumerate(authorlist.iterrows()):
     author = row[1][author_col]
+    #author_backup = row[0]
     author_backup = row[1][author_col_backup]
 
-    if author == '':
-        if author_backup == '':
+    if not isinstance(author, basestring):
+        author = str(author)
+
+    if not isinstance(author_backup, basestring):
+        author_backup = str(author_backup).strip()
+
+    if len(author) < 1:
+        if len(author_backup) < 1:
             i_blank += 1
         else:
             author = author_backup
@@ -235,10 +351,10 @@ for i_line, row in enumerate(authorlist.iterrows()):
         author = clean_email_str(author)
 
     if len(linestr) + len(author) > max_line_length_char:
-        fout.write("%s\n" % linestr)
+        fout.write("%s\n" % linestr.encode('utf-8'))
         linestr = prepend
 
-    linestr += author
+    linestr += author.strip()
 
     if i_line < n_lines - 1:
         linestr += ", "
@@ -246,8 +362,10 @@ for i_line, row in enumerate(authorlist.iterrows()):
 
 # print the last line, if there is one
 if len(linestr) > 1:
-    fout.write("%s\n" % linestr)
-
+    try:
+        fout.write("%s\n" % linestr.encode('utf-8'))
+    except:
+        fout.write("%s\n" % linestr.encode('utf8', 'replace'))
 
 
 fout.close()
@@ -259,8 +377,8 @@ if i_blank > 0:
 
 if out_logged_in:
     # if we don't set an index an extra i_index col gets printed to the file
-    authorlist.set_index(author_col, inplace=True)
-    authorlist.to_csv(outcsv)
+    #authorlist.set_index(author_col, inplace=True)
+    authorlist.to_csv(outcsv, encoding='utf-8')
     print("Logged-in-only list saved to %s." % outcsv)
 
 
