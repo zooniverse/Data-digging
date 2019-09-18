45085bebc0cdff8152a181eafa9d706dbc56464f
Will Granger <twgranger@gmail.com>
2019-09-18 10:43:31 -0500

    Update Naming Conventions

--- example_scripts/galaxy_zoo_touch_table/prepare_db_from_classification_export.py
+++ example_scripts/galaxy_zoo_touch_table/prepare_db_from_classification_export.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import json
 import string
-from tqdm import tqdm
+from tqdm import tqdm as progress_bar
 from itertools import cycle
 
 def preprocessing():
@@ -21,13 +21,15 @@ def preprocessing():
     classifications['star'] = 0;
 
     # Copy data from metadata into correct/new CSV headers with progress bar
-    with tqdm(total=len(classifications)) as pbar:
+    with progress_bar(total=len(classifications)) as current_progress:
         for index, row in classifications.iterrows():
-            pbar.update(1)
+            current_progress.update(1)
             for column in row['metadata']:
+                # Find the Right Ascension and Declination in the metadata and assign to columns
                 if column in ['ra', 'dec', '!ra', '!dec']:
-                    strippedPunctuation = column.strip(string.punctuation)
-                    classifications.loc[index, strippedPunctuation] = row['metadata'][column]
+                    stripped_punctuation = column.strip(string.punctuation)
+                    classifications.loc[index, stripped_punctuation] = row['metadata'][column]
+                # Find the image name, titled 'iauname', in the metadata and assign to a column
                 if column in ['iauname', '!iauname']:
                     classifications.loc[index, 'filename'] = row['metadata'][column]
             for column in row['locations']:
