273be6f527ad96d72f48d88d4a4d9494d2283c61
lcjohnso <lcj@northwestern.edu>
2026-05-28 00:57:51 -0500

    add comments, pretty print

--- scripts_Utility/get_workflow_stats.py
+++ scripts_Utility/get_workflow_stats.py
@@ -1,9 +1,22 @@
+###################################
+# Script: Query Workflow Stats
+#
+# To Use:
+# 1) Edit input parameters below.
+# 2) Run from command line: `python get_workflow_stats.py`
+
 import requests
+import json
 
 def main():
+
+    ###################################
+    # Input Parameters
     WORKFLOW_ID = 19276  # Replace with actual workflow ID
     START_DATE = "2026-05-20"  # Replace with start date
     END_DATE = "2026-05-26"  # Replace with end date
+    PRETTY_PRINT = True
+    ###################################
 
     # List of all possible parameters: (found in https://zooniverse.github.io/eras/querying-classification-counts-unauth.html#querying-classification-counts-unauthenticated)
     # workflow_id (or project_id, but not both), period, start_date, end_date
@@ -14,10 +27,13 @@ def main():
 
     if END_DATE:
         url += f"&end_date={END_DATE}"
+
     response = requests.get(url)
     response.raise_for_status()
-    print(response.text)
-
+    if PRETTY_PRINT:
+        print(json.dumps(response.json(), indent=2))
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
